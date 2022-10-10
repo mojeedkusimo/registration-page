9cf481d323e26c2e311de10e53d16fbc8907d07b
mojeedkusimo <mkusimo90@gmail.com>
2022-10-10 21:53:52 +0100

    Add form validation

--- script.js
+++ script.js
@@ -6,45 +6,63 @@ let address = document.querySelector('#address');
 let emailId = document.querySelector('#email');
 let password = document.querySelector('#password');
 let response = document.querySelector('#response');
+let errorMsg = document.querySelector('#error');
 
 submitButton.addEventListener(
     'click', async (e) => {
         e.preventDefault();
-
+        
+        errorMsg.innerHTML = "";
         response.innerHTML = "";
-        let requestBody = {
-            dirFirstName: dirFirstName.value,
-            dirLastName: dirLastName.value,
-            address: address.value,
-            emailId: emailId.value,
-            password: password.value,
-            reference: "reference",
-            uid: "uid",
-            businessName: "businessName",
-            title: "title",
-            dirBvn: "dirBvn",
-            dirMiddleName: "dirMiddleName",
-            userName: "userName",
-            phone: "phone",
-            city: "city",
-            customerRiskRating: "customerRiskRating",
-            tier: "tier",
-            dirDateOfBirth: "dirDateOfBirth",
-            countryOfBirth: "countryOfBirth"
+
+        
+        if (dirFirstName.value  == "") {
+            errorMsg.innerHTML = "Please fill all fields";
+        } else if ( dirLastName.value == "") {
+            errorMsg.innerHTML = "Please fill all fields";
+        } else if ( address.value.value == "") {
+            errorMsg.innerHTML = "Please fill all fields";
+        } else if ( emailId.value == "") {
+            errorMsg.innerHTML = "Please fill all fields";
+        } else if ( password.value == "") {
+            errorMsg.innerHTML = "Please fill all fields";
+        } else {
+            
+            response.innerHTML = "loading...";
+            let requestBody = {
+                dirFirstName: dirFirstName.value,
+                dirLastName: dirLastName.value,
+                address: address.value,
+                emailId: emailId.value,
+                password: "",
+                reference: "",
+                uid: "",
+                businessName: "",
+                title: "",
+                dirBvn: "",
+                dirMiddleName: "",
+                userName: "",
+                phone: "",
+                city: "",
+                customerRiskRating: "",
+                tier: "",
+                dirDateOfBirth: "",
+                countryOfBirth: ""
+            }
+    
+            let info = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody)
+            }
+    
+            let request = await fetch('https://enaira.istemlabsafrica.site/api/register/merchant', info);
+            let result = await request.json();
+    
+            response.innerHTML = result.message;
         }
-
-        let info = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        }
-
-        let request = await fetch('https://enaira.istemlabsafrica.site/api/register/merchant', info);
-        let result = await request.json();
-
-        response.innerHTML = result.message;
 
     }
 )
